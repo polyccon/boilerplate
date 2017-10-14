214a2d10e8548f59e6f63c5f205b40e3d565a2ed
Constantina <polyccon@gmail.com>
2017-10-14 23:45:57 +0100

    cookie middleware added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const path = require('path');
-
+const exphbs = require('express-handlebars');
 const controllers = require('./controllers/index');
+const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
 
-app.set('port', process.env.PORT || 3000);
+const bodyParser = require('body-parser');
+require('env2')('config.env');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    defaultLayout: 'main',
+  }));
+  app.use(cookieSession({
+    name: 'session',
+    secret: process.env.SESSION_SECRET,
+    // the session will last for three days
+    maxAge: 24 * 60 * 60 * 1000 * 3,
+  }));
+
+  app.use(cookieParser());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(controllers);
 
 module.exports = app;
