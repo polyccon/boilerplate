cec877db11bc7a9e39b0fc9e21f52d0236048ef5
Constantina <polyccon@gmail.com>
2017-12-03 18:25:30 +0000

    added contact page endpoint

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const router = express.Router();
 const home = require('./home');
+const contact = require('./contact');
 
+const router = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', home.get);
+router.get('/home', home.get);
+router.get('/contact', contact.get);
 
 module.exports = router;
